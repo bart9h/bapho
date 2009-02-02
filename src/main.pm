1e7400a4dead00a21d6b31232f1a0d18fa47d595
Rodolfo Borges <rodolfo.borges@gmail.com>
2009-02-02 01:13:51 -0200

    process all waiting events before displaying image
    also fixed first-image-display-sometimes-black bug

--- src/main.pm
+++ src/main.pm
@@ -13,18 +13,17 @@ use File::Find;
 
 use SDL::App;
 use SDL::Constants;
-use SDL::Event;
 use SDL::Surface;
 
 use args qw/%args/;
 
 #>
 
-sub display ($)
+sub display
 {#<
-	my ($self, $idx) = @_;
+	my ($self) = @_;
 
-	my $pic = $self->{pics}->{$self->{keys}->[$idx]};
+	my $pic = $self->{pics}->{$self->{keys}->[$self->{cursor}]};
 
 	state $bg = SDL::Color->new (-r => 0, -g => 0, -b => 0);
 	$self->{app}->fill (0, $bg);
@@ -38,9 +37,10 @@ sub display ($)
 	);
 	$surf->blit (0, $self->{app}, $dest);
 	$self->{app}->update;
+	$self->{app}->sync;
 }#>
 
-sub load_files()
+sub load_files
 {#<
 	my %pics = ();
 
@@ -63,7 +63,7 @@ sub load_files()
 	return \%pics;
 }#>
 
-sub get_window_geometry()
+sub get_window_geometry
 {#<
 	my ($w, $h) = (0, 0);
 
@@ -95,24 +95,16 @@ sub do ($)
 	my ($self, $event) = @_;
 	return unless defined $event;
 
-	state $cursor = 0;
-	my $oldcursor = $cursor;
-
 	given ($event) {
-		when (/image_go_next/)     { $cursor++; }
-		when (/image_go_previous/) { $cursor--; }
+		when (/image_go_next/)     { $self->{cursor}++; }
+		when (/image_go_prev/) { $self->{cursor}--; }
 		default { die }
 	}
 
 	my $last = (scalar @{$self->{keys}}) - 1;
-	$cursor = $last  if $cursor < 0;
-	$cursor = 0      if $cursor > $last;
+	$self->{cursor} = $last  if $self->{cursor} < 0;
+	$self->{cursor} = 0      if $self->{cursor} > $last;
 
-	if ($oldcursor != $cursor) {
-		$self->display ($cursor);
-		$oldcursor = $cursor;
-		$self->{app}->sync;
-	}
 }#>
 
 sub main (@)
@@ -135,33 +127,47 @@ sub main (@)
 		($args{fullscreen} ? '-fullscreen':'-resizeable') => 1,
 	);
 
+	use SDL::Event;
+	my $event = new SDL::Event;
 	SDL::Event->set_key_repeat (200, 30);
-	$self->display (0);
-	$self->{app}->loop({
+	$self->{cursor} = 0;
+	$self->display;
+	while(1)
+	{#< main loop
 
-		SDL_QUIT() => sub { exit(0); },
+		my $oldcursor = $self->{cursor};
 
-		SDL_KEYDOWN() => sub
-		{#<
-			my $event = shift;
-			given ($event->key_name) {
-				when (/^q$/) { exit(0); }
-				when (/^(space|down|right)$/)  { $self->do ('image_go_next'); }
-				when (/^(backspace|up|left)$/) { $self->do ('image_go_prev'); }
-				default { say "[$event->key_name]"; }
+		while ($event->poll)
+		{#< handle events
+
+			if ($event->type == SDL_QUIT()) {
+				exit (0);
 			}
-		},#>
+			elsif ($event->type == SDL_KEYDOWN())
+			{#<
+				given ($event->key_name) {
+					when (/^q$/) { exit(0); }
+					when (/^(space|down|right)$/)  { $self->do ('image_go_next'); }
+					when (/^(backspace|up|left)$/) { $self->do ('image_go_prev'); }
+					default { say "[$event->key_name]"; }
+				}
+			}#>
+			elsif ($event->type == SDL_MOUSEBUTTONDOWN())
+			{#<
+				$self->do ({
+						4 => 'image_go_next',
+						5 => 'image_go_prev',
+					}->{$event->button});
+			}#>
 
-		SDL_MOUSEBUTTONDOWN() => sub
-		{#<
-			my $event = shift;
-			$self->do ({
-					4 => 'image_go_next',
-					5 => 'image_go_previous',
-				}->{$event->button});
-		},#>
+		}#>
 
-	});
+		if ($oldcursor != $self->{cursor}) {
+			$oldcursor = $self->{cursor};
+			$self->display;
+		}
+
+	}#>
 
 }#>
 
