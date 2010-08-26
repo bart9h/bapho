9a794edbff1be35aa13167b4624590bce74da82d
Rodolfo Borges <rodolfo.borges@gmail.com>
2010-08-26 16:55:51 -0300

    implemented PictureItr::seek()

--- src/PictureItr.pm
+++ src/PictureItr.pm
@@ -19,18 +19,43 @@ sub new
 		files => [],
 	};
 
+	$self->pvt__collect;
+
+	print Dumper($self);
+	$self;
+}#
+
+sub seek
+{my ($self, $dir) = @_;
+
+	while($dir) {
+		my $d = $dir>0?1:-1;
+		my $id = $self->pvt__id;
+		$self->{itr}->pvt__seek($d) until $self->pvt__id ne $id;
+		$dir -= $d;
+	}
+
+	$self->pvt__collect;
+	print Dumper($self);
+	$self;
+}#
+
+sub pvt__collect
+{my ($self) = @_;
+
+	$self->{files} = [];
+
 	my $id = $self->pvt__id;
 	$self->{itr}->seek(-1) while $self->pvt__id eq $id;
 	$self->{itr}->seek(+1);
+
 	while ($self->pvt__id eq $id) {
 		push @{$self->{files}}, $self->{itr}->path;
 		$self->{itr}->seek(+1);
 	}
 	$self->{itr}->seek(-1);
-	$self->pvt__id eq $id or die;
 
-	print Dumper($self);
-	$self;
+	$self->pvt__id eq $id or die;
 }#
 
 sub pvt__id
