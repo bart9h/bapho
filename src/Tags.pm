b8fc57878d42fcc01c0809b15e97c99a2c93be63
Rodolfo Borges <rodolfo.borges@gmail.com>
2010-09-06 08:57:38 -0300

    save all tags in $basedir/.bapho-tags

--- src/Tags.pm
+++ src/Tags.pm
@@ -35,25 +35,19 @@ sub add
 {my ($self, $something) = @_;
 
 	if ($something =~ m{/}) {
-		my $path = $something;
-
-		if (open F, $path) {
+		if (open F, $something) {
 			foreach (<F>) {
 				s/^\s*(.*?)\s*$/$1/;
 				next if m/^#/;
 				next if $_ eq '';
-				$self->{tags}->{$_} = 1;
-				$all_tags{$_} = 1;
+				$self->pvt__set_tag($_);
 			}
 			close F;
 		}
 	}
 	else {
-		my $tag = $something;
-
-		$self->{tags}->{$tag} = 1;
-		$all_tags{$tag} = 1;
-		$self->pvt__save_tags;
+		$self->pvt__set_tag($something);
+		$self->pvt__save_pic_tags;
 	}
 }#
 
@@ -64,23 +58,43 @@ sub toggle
 		delete $self->{tags}->{$tag};
 	}
 	else {
-		$self->{tags}->{$tag} = 1;
-		$all_tags{$tag} = 1;
+		$self->pvt__set_tag($tag);
 	}
 
-	$self->pvt__save_tags;
+	$self->pvt__save_pic_tags;
+}#
+
+sub pvt__set_tag
+{my ($self, $tag) = @_;
+
+	$self->{tags}->{$tag} = 1;
+	unless (defined $all_tags{$tag}) {
+		$all_tags{$tag} = 1;
+		$self->pvt__save_all_tags;
+	}
+}#
+
+sub pvt__save_pic_tags
+{my ($self) = @_;
+
+	pvt__save_tags($self->{id}.'.tags', $self->{tags});
+}#
+
+sub pvt__save_all_tags
+{my ($self) = @_;
+
+	pvt__save_tags($args{basedir}.'/.bapho-tags', \%all_tags);
 }#
 
 sub pvt__save_tags
-{my ($self) = @_;
+{my ($filename, $tags) = @_;
 
 	unless ($args{nop}) {
-		my $filename = $self->{id}.'.tags';
 
-		if (scalar keys %{$self->{tags}} > 0) {
+		if (scalar keys %$tags > 0) {
 			open F, '>', $filename  or die "$filename: $!";
 			say "saving $filename"  if $args{verbose};
-			print F "$_\n"  foreach sort keys %{$self->{tags}};
+			print F "$_\n"  foreach sort keys %$tags;
 			close F;
 		}
 		else {
