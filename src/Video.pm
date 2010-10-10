c4bc51bb93f9ec72bcd0c8493ce85b2630deb634
Rodolfo Borges <rodolfo.borges@gmail.com>
2010-10-10 09:13:41 -0300

    fill roll frame for video snapshots

--- src/Video.pm
+++ src/Video.pm
@@ -8,6 +8,34 @@ use SDL::Surface;
 use args qw/%args dbg/;
 #}#
 
+sub render_film_roll_frame
+{my ($surf) = @_;
+
+	my $b = int($surf->height/(3*15+1));
+	my $w = 1;  # width of the white line on the borders
+
+	my $r = SDL::Rect->new(-x => 0, -y => 0, -width => $w, -height => $surf->height);
+	$surf->fill($r, $SDL::Color::white);
+	$r->x($surf->width - 1);
+	$surf->fill($r, $SDL::Color::white);
+
+	foreach my $x (0+$w, $surf->width-1 -($w+5*$b)) {
+		$r->x($x);
+		$r->y(0);
+		$r->width(5*$b);
+		$r->height($surf->height);
+		$surf->fill($r, $SDL::Color::black);
+
+		$r->x($x+$b);
+		$r->width(3*$b);
+		$r->height(2*$b);
+		for (my $y = $b; $y < $surf->height; $y += 3*$b) {
+			$r->y($y);
+			$surf->fill($r, $SDL::Color::white);
+		}
+	}
+}#
+
 sub load_sample_frame
 {my ($videofile) = @_;
 
@@ -27,6 +55,7 @@ sub load_sample_frame
 	}
 	rmdir $tmpdir or die;
 
+	render_film_roll_frame($surf) if $surf;
 	$surf;
 }#
 
