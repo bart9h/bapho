a5be350fbb9b9a841b89af5e91c56bb31eb2cd6a
Rodolfo Borges <rodolfo.borges@gmail.com>
2010-11-07 08:43:31 -0200

    test routine for FileItr

--- src/FileItr.pm
+++ src/FileItr.pm
@@ -78,5 +78,50 @@ sub read_directory
 	return ();
 }#
 
+sub test
+{#{my test}
+	use v5.10;
+	my $i = FileItr->new($_[0] // $ENV{PWD});
+	my $dir = 1;
+	while(1) {
+		print ">>> $i->{path} >>> ";
+		local $_ = <STDIN>;
+		chomp;
+		given ($_) {
+			when (/^(help|\?)$/) {
+				say 'q=quit, <[+/-]number>=direction, n|j=+1, p|k=-1';
+				next;
+			}
+			when (/^$/) {
+			}
+			when (/^up$/) {
+				$i = $i->up;
+				next;
+			}
+			when (/^down$/) {
+				$i = $i->down;
+				next;
+			}
+			when (/^q$/) {
+				last;
+			}
+			when (/^(n|j)$/) {
+				$dir = +1;
+			}
+			when (/^(p|k)$/) {
+				$dir = -1;
+			}
+			when (/^([+-]?\d+)$/) {
+				$dir = $1;
+			}
+			default {
+				say '?';
+				next;
+			}
+		}
+		$i = $i->seek($dir) or say "seek failed";
+	}
+}#
+
 1;
 # vim600:fdm=marker:fmr={my,}#:
